0d3cdf45f2752fece856dc8701e17ae549e53498
Keongy <info.pichon@gmail.com>
2022-01-23 16:24:11 +0100

    Add default lang from browser lang

--- src/context/langContext.js
+++ src/context/langContext.js
@@ -2,13 +2,22 @@ import { createContext, useState } from "react";
 
 export const Context = createContext();
 
+let browserLang = navigator.language.slice(0, 2).toUpperCase()
+const supportedLangs = ['FR', 'EN', 'ES']
+
+if (supportedLangs.indexOf(browserLang) === -1) {
+    console.log("not supported");
+    browserLang = 'EN'
+}
+
 const ContextProvider = props => {
 
-    const [lang, setLang] = useState('EN');
+    const [lang, setLang] = useState(browserLang);
 
     const toggleLang = changeLang => {
         setLang(changeLang)
     }
+
 
 
     return (
